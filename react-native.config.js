8b9cc04ac3b539eb5d31bfefe2fb63c669f7fbe7
延飞 李 <liyanfei_2011@163.com>
2023-06-08 13:33:42 +0800

    no message

---
+++ react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  dependency: { platforms: { android: { sourceDir: "lib/android" } } },
+  dependencies: {
+    "React-native-amap-geolocation-fg": {
+      root: __dirname,
+      platforms: {
+        android: {
+          sourceDir: __dirname + "/lib/android",
+          packageImportPath: "import cn.qiuxiang.react.geolocation.AMapGeolocationPackage;",
+          packageInstance: "new AMapGeolocationPackage()",
+        },
+      },
+    },
+  },
+};
