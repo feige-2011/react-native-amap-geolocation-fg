e20084f575fa62109654efca69b458a19683b6d8
延飞 李 <liyanfei_2011@163.com>
2023-06-12 14:39:42 +0800

    no message

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  dependency: { platforms: { android: { sourceDir: "lib/android" } } },
+  dependency: { platforms: { ios: { project: "lib/ios/react-native-amap-geolocation-fg.podspec" },android: { sourceDir: "lib/android" } } },
   dependencies: {
     "React-native-amap-geolocation-fg": {
       root: __dirname,
       platforms: {
+       ios: { podspecPath: __dirname + "/lib/ios/react-native-amap-geolocation-fg.podspec" },
         android: {
           sourceDir: __dirname + "/lib/android",
           packageImportPath: "import cn.lyf.react.geolocation.BDGeolocationPackage;",
